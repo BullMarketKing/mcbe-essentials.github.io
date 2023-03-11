6cb86f91c96718a0a866394f579fb72de1ec551e
BullMarketKing <bullmarketking@outlook.com>
2023-03-10 21:08:15 -0600

    Handle item.Damage and item.tag attributes.

--- structure-editor/loot-tabler/script_structFix.js
+++ structure-editor/loot-tabler/script_structFix.js
@@ -208,26 +208,22 @@ function sf_makeLootTable(cTile, cName) {
   var ltPools = oLootTable.pools;
   var storageLocation = cTile.value.block_entity_data.value.Items.value.value;
   for(var i = 0; i < storageLocation.length; i++){
-    var itemName = storageLocation[i].Name.value;
-    var itemCount = storageLocation[i].Count.value;
-    var itemSlot = storageLocation[i].Slot.value;
+    var item = storageLocation[i];
+    var itemName = item.Name.value;
+    var itemCount = item.Count.value;
+    var itemSlot = item.Slot.value;
 
     // Create pool.
-    ltPools[itemSlot] = {
-      rolls: 1,
-      entries: [
-        {
-          type: "item",
-          name: itemName,
-          functions: [
-            {
-              function: "set_count",
-              count: itemCount
-            }
-          ]
-        }
-      ]
-    };
+    ltPool = { rolls: 1, entries: [
+      { type: 'item', name: itemName, functions: [
+          { function: 'set_count', count: itemCount }
+        ]
+      }
+    ]};
+
+    sf_genItemAttrsInLootTablePool(item, ltPool);
+
+    ltPools[itemSlot] = ltPool;
   }
 
   // Create pools for empty slots.
@@ -235,17 +231,193 @@ function sf_makeLootTable(cTile, cName) {
     if(ltPools[i]) {
       continue;
     }
-    ltPools[i] = {
-      rolls: 1,
-      entries: [
-        {
-          type: "empty"
-        }
-      ]
-    };
+    ltPools[i] = { rolls: 1, entries: [{ type: 'empty' }] };
   }
 
   return oLootTable;
+}
+
+function sf_genItemAttrsInLootTablePool(nbtItem, ltPool) {
+  var itemName = nbtItem.Name.value;
+  var ltFunctions = ltPool.entries[0].functions; 
+
+  // Handle item.Damage.
+
+  if(nbtItem.Damage && nbtItem.Damage.value != 0) {
+    var damage = nbtItem.Damage.value;
+    if(('minecraft:potion'
+      + ' minecraft:lingering_potion'
+      + ' minecraft:splash_potion'
+      + ' minecraft:arrow'
+      + ' minecraft:banner'
+      + ' minecraft:empty_map'
+      + ' minecraft:goat_horn'
+      + ' minecraft:skull').indexOf(itemName) != -1) {
+      // These items have been verified.
+      ltFunctions.push({
+        function: 'set_data',
+        data: damage       
+      });
+      console.log('item.Damage handled by set_dat: value=' + damage + ' ' + itemName);  
+    }   
+    else if(('minecraft:firework_star').indexOf(itemName) != -1) {
+      // These items do not seem to react to set_data.
+      ltFunctions.push({
+        function: 'set_data',
+        data: damage       
+      });
+      console.log('item.Damage handled by set_dat: value=' + damage + ' ' + itemName);  
+    }       
+    else {
+      // These items have not been verified.
+      ltFunctions.push({
+        function: 'set_data',
+        data: damage       
+      });
+      console.warn('item.Damage handled by set_dat: value=' + damage + ' ' + itemName);  
+    } 
+  }
+  
+  // Handle tag.
+
+  if(typeof(nbtItem.tag) === 'undefined') {
+    // No attributes to handle.
+    return;
+  }
+
+  if(nbtItem.tag.type !== 'compound'
+    || typeof(nbtItem.tag.value) === 'undefined') {
+    console.warn('handleTag: Invalid tag format. Item:' + itemName);
+    return;
+  }
+  
+  var nbtTag = nbtItem.tag.value;
+  
+  for(const nbtAttrName in nbtTag) {
+    const nbtAttr = nbtTag[nbtAttrName];
+    if(nbtAttrName === 'ench') {
+      try {
+        ltFunctions.push({
+          function: 'specific_enchants',
+          enchants: nbtAttr.value.value.map(x => ({
+            id: sf_getEnchantmentName(x.id.value),
+            level: x.lvl.value
+          }))
+        });
+        console.log('Enchanted: ' + itemName);
+        if(nbtAttr.value.value.length > 1) {
+          console.log('Multi-Enchanted: ' + itemName);
+        }
+      }
+      catch (err) {
+        throw 'Errored on tag attr: ' + nbtAttrName + err;
+      }
+    }
+    else if(nbtAttrName === 'Damage') {
+      try {
+        if(nbtAttr.value > 0.0 && nbtAttr.value < 1.0) {
+          // Only set if Damage value makes sense.
+          ltFunctions.push({
+            function: 'set_damage',
+            damage: nbtAttr.value
+          });
+          console.warn('tag.Damage added: value=' + nbtAttr.value + ' ' + itemName);
+        }
+        else if (nbtAttr.value != 0) {
+          console.warn('tag.Damage value out of range. value=' + nbtAttr.value + ' ' + itemName);
+        }
+        else {
+          console.log('tag.Damage = 0. ' + itemName);
+        }
+      }
+      catch (err) {
+        throw 'Errored on tag attr: ' + nbtAttrName + err;
+      }
+    }
+    else if(nbtAttrName === 'customColor') {
+      try {
+        if(('minecraft:leather_helmet'
+          + ' minecraft:leather_chestplate'
+          + ' minecraft:leather_leggings'
+          + ' minecraft:leather_boots').indexOf(itemName) != -1) {
+          if(nbtAttr.value) {
+            ltFunctions.push({
+              function: 'random_dye'
+            });
+            console.log('customColor-random_dye added:' + itemName);
+          }
+        }
+        else if(('minecraft:firework_star').indexOf(itemName) != -1) {
+          if(nbtAttr.value) {
+            ltFunctions.push({
+              function: 'random_dye'
+            });
+            console.warn('customColor-random_dye added to fireworks:' + itemName);
+          }
+        }
+        else {
+          console.warn('customColor: cannot handle item:' + itemName);
+        }
+      }
+      catch (err) {
+        throw 'Errored on tag attr: ' + nbtAttrName + err;
+      }
+    }
+    else if ('Fireworks FireworksItem'.indexOf(nbtAttrName) != -1) {
+      console.warn('handleTag: Cannot handle tag attr: ' + nbtAttrName + ' item:' + itemName, nbtTag);      
+    }
+    else {
+      console.warn('handleTag: Unexpected tag attr: ' + nbtAttrName + ' item:' + itemName, nbtTag);
+      debugger
+    }
+  }
+}
+
+function sf_getEnchantmentName(id) {
+  const enchants = {
+    protection: 0,
+    fire_protection: 1,
+    feather_falling: 2,
+    blast_protection: 3,
+    projectile_protection: 4,
+    thorns: 5,
+    respiration: 6,
+    depth_strider: 7,
+    aqua_affinity: 8,
+    sharpness: 9,
+    smite: 10,
+    bane_of_arthropods: 11,
+    knockback: 12,
+    fire_aspect: 13,
+    looting: 14,
+    efficiency: 15,
+    silk_touch: 16,
+    unbreaking: 17,
+    fortune: 18,
+    power: 19,
+    punch: 20,
+    flame: 21,
+    infinity: 22,
+    luck_of_the_sea: 23,
+    lure: 24,
+    frost_walker: 25,
+    mending: 26,
+    binding: 27,
+    vanishing: 28,
+    impaling: 29,
+    loyalty: 31,
+    channeling: 32,
+    multishot: 33,
+    piercing: 34,
+    quick_charge: 35,
+    soul_speed: 36
+  };  
+  for(var name in enchants) {
+    if(enchants[name] == id) {
+      return name;
+    }
+  }
+  throw 'Unknown enchantment ID: ' + id;
 }
 
 function sf_nameContainers(cTiles) {
